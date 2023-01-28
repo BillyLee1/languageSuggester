385feb33220d74b657be5e299bc7dc42947bc608
Billy Lee <codingbillylee@gmail.com>
2023-01-28 15:01:43 -0800

    Add feature to hide answer until submit is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function handleSurvey(event) {
   event.preventDefault()
+  hideAns();
+
   const q1 = document.querySelector("input[name='bool']:checked").value;
   const q2 = document.querySelector("input[name='interests']:checked").value;
   const q4 = document.querySelector("input[name='mobileDevice']:checked").value;
@@ -7,10 +9,9 @@ function handleSurvey(event) {
   surveyOutput(q1, q2, q4);
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  const submit = document.getElementById("btn");
-  submit.addEventListener("click", handleSurvey);
-})
+function hideAns() {
+  document.querySelector("#answer").setAttribute("class", "hidden");
+}
 
 function surveyOutput(q1, q2, q4) {
   let ans = document.querySelector(".langChoice");
@@ -27,8 +28,15 @@ function surveyOutput(q1, q2, q4) {
       } else {
         ans.innerText = "Java";
       }
+      
     }
   } else {
-    ans.innerText = "Probably shouldn't code then. :(";
+    ans.innerText = "Probably shouldn't code since you don't want to learn. :(";
   }
+  document.querySelector(".hidden").removeAttribute("class");
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  const submit = document.getElementById("btn");
+  submit.addEventListener("click", handleSurvey);
+})
